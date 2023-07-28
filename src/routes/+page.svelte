16e83f924452a103f148292e4a97f64d05056285
Jackson <15jgme@queensu.ca>
2023-07-28 12:06:03 -0400

    Correct promotions for user and bot. Make engine data visable to user

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,7 @@
   import { Chess, DEFAULT_POSITION, validateFen } from "chess.js";
   import "$lib/assets/styles/cm-chessboard.css";
   import "$lib/assets/extensions/promotion-dialog/promotion-dialog.css";
-  import "$lib/assets/extensions/markers/markers.css"
+  import "$lib/assets/extensions/markers/markers.css";
   import { onMount } from "svelte";
 
   import { invoke } from "@tauri-apps/api/tauri";
@@ -24,6 +24,7 @@
     Pondering = "🤔",
     Waiting = "😁",
   }
+  let is_checkmate: Boolean = false;
   let curr_engine_state_emoj: engine_state_emoji = engine_state_emoji.Waiting;
   let fen_error_msg: String = "";
 
@@ -32,6 +33,9 @@
   let fen: string = "8/1pn5/5k2/8/6P1/3K1N2/8/8 w - - 0 1";
   fen = DEFAULT_POSITION;
   let engine_move: string = "";
+
+  // Engine stats
+  let engine_stats: any;
 
   // ChessJS
   let chess = new Chess(fen);
@@ -50,11 +54,17 @@
     // Validate fen first
     let { ok, error } = validateFen(fen);
 
-    if (ok) { 
-      fen_error_msg = "" // Erase error message
+    if (ok) {
+      fen_error_msg = ""; // Erase error message
+
+      // Reset stats
+      engine_stats = undefined;
 
       // Reset engine emoji
       curr_engine_state_emoj = engine_state_emoji.Waiting;
+
+      // Reset checkmate indicator
+      is_checkmate = false;
 
       // Reset ChessJS
       chess = new Chess(fen);
@@ -71,22 +81,36 @@
       // board = new Chessboard(board_el, board_config);
       board.setPosition(chess.fen(), true);
       board.enableMoveInput(inputHandler, COLOR.white);
-    } else if (error){
+    } else if (error) {
       fen_error_msg = error;
     } else {
-      fen_error_msg = "invalid fen"
+      fen_error_msg = "invalid fen";
     }
   }
 
   async function makeEngineMove(chessboard: any) {
     let board_fen = chess.fen();
-    let move_str: string = await invoke("run_engine", {
+    let eng_res: any = await invoke("run_engine", {
       currentFen: board_fen,
     });
-    let move_from = move_str.substring(0, 2);
-    let move_to = move_str.substring(2);
 
-    let move_obj = { from: move_from, to: move_to };
+    engine_stats = eng_res.engine_stats;
+
+    // Parse move
+    let engine_move = eng_res.engine_move;
+
+    let move_from = engine_move.substring(0, 2);
+    let move_to = engine_move.substring(2, 4);
+
+    let move_obj = { from: move_from, to: move_to }; // Initial object
+
+    // Add promotion data if needed
+    if (engine_move.length > 4) {
+      // Indicates promotion
+      let promotion_piece = engine_move[4]; // Grab the character at the last position
+      move_obj.promotion = promotion_piece;
+    }
+
     console.log(move_obj);
     chess.move(move_obj);
     chessboard.setPosition(chess.fen(), true);
@@ -94,6 +118,9 @@
   }
 
   async function inputHandler(event: any) {
+    // Check board state at user input
+    is_checkmate = chess.isGameOver();
+
     console.log("inputHandler", event);
     if (event.type !== INPUT_EVENT_TYPE.moveInputFinished) {
       event.chessboard.removeMarkers(MARKER_TYPE.dot);
@@ -119,11 +146,11 @@
         to: event.squareTo,
         promotion: event.promotion,
       };
-      
+
       let result = undefined;
       try {
         result = chess.move(move);
-      } catch(e) {
+      } catch (e) {
         console.log(e);
       }
 
@@ -134,6 +161,8 @@
             // update position, maybe castled and wait for animation has finished
             curr_engine_state_emoj = engine_state_emoji.Pondering;
             makeEngineMove(event.chessboard).then(() => {
+              // Check board state after engine move
+              is_checkmate = chess.isGameOver();
               curr_engine_state_emoj = engine_state_emoji.Waiting;
             });
           });
@@ -158,7 +187,11 @@
                     promotion: result.piece.charAt(1),
                   });
                   event.chessboard.setPosition(chess.fen(), true);
-                  makeEngineMove(event.chessboard).then(() => {});
+                  makeEngineMove(event.chessboard).then(() => {
+                    // Check board state after engine move
+                    is_checkmate = chess.isGameOver();
+                    curr_engine_state_emoj = engine_state_emoji.Waiting;
+                  });
                 } else {
                   // promotion canceled
                   event.chessboard.enableMoveInput(inputHandler, COLOR.white);
@@ -169,11 +202,15 @@
             return true;
           }
         }
+        console.log(event.chessboard);
       }
       return result;
     } else if (event.type === INPUT_EVENT_TYPE.moveInputFinished) {
       if (event.legalMove) {
         event.chessboard.disableMoveInput();
+        console.log(event);
+      } else {
+        console.log("Illegal move");
       }
     }
   }
@@ -187,7 +224,14 @@
 </script>
 
 <div class="p-2 flex flex-col items-center justify-center h-screen">
-  <div><p>Engine state: {curr_engine_state_emoj}</p></div>
+  <div>
+    {#if !is_checkmate}
+      <p>Engine state: {curr_engine_state_emoj}</p>
+    {:else}
+      <p>Game over</p>
+    {/if}
+  </div>
+
   <div class="w-1/2" bind:this={board_el} />
 
   <div class="flex space-x-2 py-2">
@@ -204,12 +248,24 @@
   </div>
   <p class="font-extralight text-red-300">{fen_error_msg}</p>
   {#if fen_error_msg != ""}
-    <button class="bg-red-500 hover:bg-red-700 text-white font-bold px-2 rounded" on:click={() => {fen = DEFAULT_POSITION; reload_chess()}}>Total FEN reset?</button>
+    <button
+      class="bg-red-500 hover:bg-red-700 text-white font-bold px-2 rounded"
+      on:click={() => {
+        fen = DEFAULT_POSITION;
+        reload_chess();
+      }}>Total FEN reset?</button
+    >
+  {/if}
+
+  {#if engine_stats}
+    <div>
+      <p>
+        Depth reached: {engine_stats.depth_reached}, Nodes evaluated: {engine_stats.searched_nodes},
+        α-β reduction: {(
+          100 -
+          (engine_stats.searched_nodes / engine_stats.all_nodes) * 100
+        ).toFixed(2)}
+      </p>
+    </div>
   {/if}
 </div>
-
-<!-- <style>
-  #filldiv {
-    
-  }
-</style> -->
